8d933e7d8341d843c99b7d6d92ff458e35deffcb
dhlmx <46362369+dhlmx@users.noreply.github.com>
2024-03-20 17:19:42 -0600

    Fix: CI/CD: Build [11]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/graphs',
+    redirectTo: '/bases',
     pathMatch: 'full'
   },
   {
@@ -33,7 +33,7 @@ const routes: Routes = [
   },
   {
     path: '**',
-    redirectTo: '/graphs'
+    redirectTo: '/bases'
   }
 ];
 
